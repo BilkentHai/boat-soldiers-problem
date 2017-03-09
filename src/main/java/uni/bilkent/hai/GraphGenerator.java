01f0eb39aa78f2c776ea34c5cb8f75cefc9e7a6d
eliztekcan <eliztekcan@gmail.com>
2017-03-09 19:54:20 +0300

    Minor changes in GraphGenerator

--- src/main/java/uni/bilkent/hai/GraphGenerator.java
+++ src/main/java/uni/bilkent/hai/GraphGenerator.java
@@ -1,8 +1,6 @@
 package uni.bilkent.hai;
-
 import java.util.ArrayList;
 import java.util.List;
-
 /**
  * Created by eliztekcan on 9.03.2017.
  */
@@ -25,23 +23,36 @@ public class GraphGenerator {
             neighborstates = curState.getNeighborStates();
             nodes += "\"data\": { \"id\": \"" + curState.getid() + "\",";
             nodes += "\"content\": \"((" ;
+
             if(curState.getStart().getNumberOfBoys() != 0)
             {
                 nodes += curState.getStart().getNumberOfBoys() + " Boy";
             }
+
             if(curState.getStart().getNumberOfSoldiers() != 0)
             {
                 nodes += "," + curState.getStart().getNumberOfSoldiers() + " Soldier";
             }
+
+            if( curState.getBoat().getRiverSide() ==RiverSide.START){
+                nodes += ", Boat";
+            }
             nodes += ") (";
+
             if(curState.getGoal().getNumberOfBoys() != 0)
             {
                 nodes += curState.getGoal().getNumberOfBoys() + " Boy";
             }
+
             if(curState.getGoal().getNumberOfSoldiers() != 0)
             {
                 nodes += "," + curState.getGoal().getNumberOfSoldiers() + " Soldier";
             }
+
+            if( curState.getBoat().getRiverSide() ==RiverSide.GOAL){
+                nodes += ", Boat";
+            }
+
             nodes += "))\" } }";
             if(i != states.size()-1)
             {
@@ -49,16 +60,18 @@ public class GraphGenerator {
             }
             for(int j = 0; j < neighborstates.size(); j++) {
                 edges += "\"data\": { \"source\": \"" + curState.getid() + "\",\"target\": "
-                        + neighborstates.get(j).getid() + "\" } }";
-                if(j != neighborstates.size()-1)
-                {
-                    edges += ",";
-                }
+                        + neighborstates.get(j).getid() + "\" } },";
             }
+            edges = edges.substring(0, edges.length()-1);
+
         }
         nodes += "]";
         edges += "]";
-
-
     }
+    public static void main(String[] args) {
+        GraphGenerator g = new GraphGenerator();
+        System.out.println(g.nodes);
+        System.out.println(g.edges);
+    }
+
 }
