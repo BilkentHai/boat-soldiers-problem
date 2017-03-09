2b3c237c2b2ba31463c4c2ef59a719b0b582af47
deterjan <d.ulusel@gmail.com>
2017-03-09 20:36:26 +0300

    Minor changes to graph generator

--- src/main/java/uni/bilkent/hai/StateGenerator.java
+++ src/main/java/uni/bilkent/hai/StateGenerator.java
@@ -11,46 +11,15 @@ public class StateGenerator {
     private int numberOfBoys = 2;
     private int numberOfSoldiers = 3;
 
-    private List<State> states = new ArrayList<State>();
+    private List<State> states = new ArrayList<>();
 
 
-    public StateGenerator() {
-
+    public StateGenerator()
+    {
         for (int i = 0; i <= numberOfBoys; i++)
             for (int j = 0; j <= numberOfSoldiers; j++)
                 for (RiverSide boatSide : RiverSide.values())
                     states.add(new State(numberOfBoys - i, numberOfSoldiers - j, i, j, boatSide));
-
-
-        for (int i = 0; i < states.size(); i++) {
-            for (int j = i + 1; j < states.size(); j++) {
-                State s1 = states.get(i);
-                State s2 = states.get(j);
-
-                int boys = Math.abs(s1.getGoal().getNumberOfBoys() - s2.getGoal().getNumberOfBoys());
-                int soldiers = Math.abs(s1.getGoal().getNumberOfSoldiers() - s2.getGoal().getNumberOfSoldiers());
-
-                if (s1.getBoat().getRiverSide() == s2.getBoat().getRiverSide())
-                    continue;
-                else if (s2.getBoat().getRiverSide() == RiverSide.GOAL && (s2.getGoal().getNumberOfSoldiers() - s1.getGoal().getNumberOfSoldiers() < 0
-                        || s2.getGoal().getNumberOfBoys() - s1.getGoal().getNumberOfBoys() < 0))
-                    continue;
-                else if (s2.getBoat().getRiverSide() == RiverSide.START && (s2.getStart().getNumberOfSoldiers() - s1.getStart().getNumberOfSoldiers() < 0
-                        || s2.getStart().getNumberOfBoys() - s1.getStart().getNumberOfBoys() < 0))
-                    continue;
-                else if ((boys <= 2 && boys >= 1 && soldiers == 0) || (boys == 0 && soldiers == 1)){
-                    s1.addNeighborState(s2);
-                    s2.addNeighborState(s1);
-                    System.out.println("State " + i);
-                    System.out.println(s1);
-                    System.out.println("State " + j);
-                    System.out.println(s2);
-                    System.out.println("-----------");
-                }
-            }
-        }
-
-
     }
 
     public int getNumberOfBoys() {
@@ -77,8 +46,48 @@ public class StateGenerator {
         this.states = states;
     }
 
-    public static void main(String[] args) {
-        new StateGenerator();
+    public String toString()
+    {
+        String result = "";
+
+        for (int i = 0; i < states.size(); i++)
+        {
+            for (int j = i + 1; j < states.size(); j++)
+            {
+                State s1 = states.get(i);
+                State s2 = states.get(j);
+
+                int boys = Math.abs(s1.getGoal().getNumberOfBoys() - s2.getGoal().getNumberOfBoys());
+                int soldiers = Math.abs(s1.getGoal().getNumberOfSoldiers() - s2.getGoal().getNumberOfSoldiers());
+
+                if (s1.getBoat().getRiverSide() == s2.getBoat().getRiverSide())
+                    continue;
+                else if (s2.getBoat().getRiverSide() == RiverSide.GOAL && (s2.getGoal().getNumberOfSoldiers() - s1.getGoal().getNumberOfSoldiers() < 0
+                        || s2.getGoal().getNumberOfBoys() - s1.getGoal().getNumberOfBoys() < 0))
+                    continue;
+                else if (s2.getBoat().getRiverSide() == RiverSide.START && (s2.getStart().getNumberOfSoldiers() - s1.getStart().getNumberOfSoldiers() < 0
+                        || s2.getStart().getNumberOfBoys() - s1.getStart().getNumberOfBoys() < 0))
+                    continue;
+                else if ((boys <= 2 && boys >= 1 && soldiers == 0) || (boys == 0 && soldiers == 1))
+                {
+                    s1.addNeighborState(s2);
+                    s2.addNeighborState(s1);
+                    result += ("State " + i + "\n");
+                    result += (s1.toString() + "\n");
+                    result += ("State " + j + "\n");
+                    result += (s2.toString() + "\n");
+                    result += ("-----------" + "\n");
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        StateGenerator sg = new StateGenerator();
+        System.out.println( sg);
     }
 
 }
